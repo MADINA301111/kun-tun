c041451ca73b5caedb9afd7d090ff32a4be0721e
Madina <madina@.gimael.com>
2023-12-22 11:07:45 +0500

    add files

---
+++ script.js
@@ -0,0 +1,13 @@
+let header = document.querySelector("header");
+let btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    if (btn.innerHTML == "Tun") {
+        btn.innerHTML = "kun";
+        header.classList.add("tun");
+        header.classList.remove("kun");
+    } else {
+        btn.innerHTML = "Tun";
+        header.classList.add("kun");
+        header.classList.remove("tun");
+    }
+});
